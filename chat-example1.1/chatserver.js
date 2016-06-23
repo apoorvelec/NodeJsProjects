b6a603e3b9e6ce31f1391e937b3396d633d9e3b9
apoorvelec <apoorv@iitgn.ac.in>
2016-06-22 21:42:03 -0400

    Added button and form

--- chat-example1.1/chatserver.js
+++ chat-example1.1/chatserver.js
@@ -5,13 +5,13 @@ response.writeHead(200, {"Content-Type": "text/html"});
     response.write("<html>");
       response.write("<head>");
         response.write("<title>Hello World Page</title>");
+	response.write("<script>function displayMessage(text){alert(text)}</script>");
 	  response.write("</head>");
 	    response.write("<body>");
-	      response.write("Hello World!");
+	      response.write("<form><input name=\"text\" type=\"text\" /><input name=\"button\" type=\"submit\" onClick=\"displayMessage(text)\"></form>");
 	        response.write("</body>");
 		  response.write("</html>");
 		    response.end();
 });
-
 server.listen(3000);
 console.log("Server listening");
