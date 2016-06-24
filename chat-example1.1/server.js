777f8810c8ec918a2d8a995510ccc7838ad6e5e5
apoorvelec <apoorv@iitgn.ac.in>
2016-06-24 15:35:52 -0400

    added functionality for calculating the number of connected users

--- chat-example1.1/server.js
+++ chat-example1.1/server.js
@@ -2,6 +2,7 @@ var http = require("http");
 var fs = require("fs");
 
 var portNumber = 3000;
+var numberOfClients = 0;
 var server = http.createServer(function(request,response){
 	fs.readFile("client.html","utf-8",function(err,data){
 		response.writeHead(200,{'Content-Type':'text/html'});
@@ -16,4 +17,17 @@ io.sockets.on("connection",function(socket){
 	socket.on("message_to_server",function(data){
 		io.sockets.emit("message_to_client",{message:data["message"]});
 	});
+
+	setInterval(function(){
+		io.sockets.emit("connected_users",numberOfClients);
+	},1000);
+});
+
+io.sockets.on("connection",function(socket){
+	numberOfClients++;
+	socket.on('disconnect',function(){
+		numberOfClients--;
+		console.log(numberOfClients);
+	});
+	console.log(numberOfClients);
 });
