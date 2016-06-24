a393252350f3a1a401ee7f85085d77013635c788
apoorvelec <apoorv@iitgn.ac.in>
2016-06-24 13:32:30 -0400

    server updated

--- chat-example1.1/server.js
+++ chat-example1.1/server.js
@@ -7,3 +7,11 @@ var server = http.createServer(function(request,response){
 	response.write("Hello World!!");
 	response.end();
 }).listen(portNumber);
+
+var io = require("socket.io").listen(server);
+
+io.sockets.on("connection",function(socket){
+	socket.on("message_to_server",function(data){
+		io.sockets.emit("message_to_client",{message:data["message"]});
+	});
+});
