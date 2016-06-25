8545120766751a7daede2f06edad5a7b3267bf37
apoorvelec <apoorv@iitgn.ac.in>
2016-06-25 15:46:58 -0400

    added functionality for showing online users

--- chat-example1.1/server.js
+++ chat-example1.1/server.js
@@ -3,6 +3,7 @@ var fs = require("fs");
 
 var portNumber = 3000;
 var numberOfClients = 0;
+var clients = [];
 var server = http.createServer(function(request,response){
 	fs.readFile("client.html","utf-8",function(err,data){
 		response.writeHead(200,{'Content-Type':'text/html'});
@@ -19,15 +20,25 @@ io.sockets.on("connection",function(socket){
 	});
 
 	setInterval(function(){
-		io.sockets.emit("connected_users",numberOfClients);
+		io.sockets.emit("connected_users",{numberOfClients:numberOfClients,clients:clients});
 	},1000);
 });
 
+// This block keeps information about the number of clients
+// It also keeps a list of socket ids for the clients in clients array
 io.sockets.on("connection",function(socket){
+	clients.push(socket.id);
 	numberOfClients++;
 	socket.on('disconnect',function(){
+		var indexOfSocket = clients.indexOf(socket.id);
+		if(indexOfSocket != -1){
+			// remove the object using splice method
+			clients.splice(indexOfSocket,1);
+		}
 		numberOfClients--;
 		console.log(numberOfClients);
+		console.log(clients);
 	});
 	console.log(numberOfClients);
+	console.log(clients);
 });
