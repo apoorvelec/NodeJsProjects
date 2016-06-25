49a2038e09006300a51059d9694e4f13152a132f
apoorvelec <apoorv@iitgn.ac.in>
2016-06-25 17:46:01 -0400

    chat with self chosen usernames

--- chat-example1.1/server.js
+++ chat-example1.1/server.js
@@ -4,6 +4,8 @@ var fs = require("fs");
 var portNumber = 3000;
 var numberOfClients = 0;
 var clients = [];
+
+
 var server = http.createServer(function(request,response){
 	fs.readFile("client.html","utf-8",function(err,data){
 		response.writeHead(200,{'Content-Type':'text/html'});
@@ -16,21 +18,20 @@ var io = require("socket.io").listen(server);
 
 io.sockets.on("connection",function(socket){
 	socket.on("message_to_server",function(data){
-		io.sockets.emit("message_to_client",{message:data["message"]});
+		io.sockets.emit("message_to_client",{message:data["message"], username:socket.username});
 	});
 
-	setInterval(function(){
-		io.sockets.emit("connected_users",{numberOfClients:numberOfClients,clients:clients});
-	},1000);
-});
+	socket.on("assign_username",function(data){
+		socket.username = data['USERNAME'];
+		console.log(socket.username+" connected ...");
+		clients.push(socket.username);
+		numberOfClients++;
+		console.log(numberOfClients);
+		console.log(clients);
+	});
 
-// This block keeps information about the number of clients
-// It also keeps a list of socket ids for the clients in clients array
-io.sockets.on("connection",function(socket){
-	clients.push(socket.id);
-	numberOfClients++;
 	socket.on('disconnect',function(){
-		var indexOfSocket = clients.indexOf(socket.id);
+		var indexOfSocket = clients.indexOf(socket.username);
 		if(indexOfSocket != -1){
 			// remove the object using splice method
 			clients.splice(indexOfSocket,1);
@@ -39,6 +40,9 @@ io.sockets.on("connection",function(socket){
 		console.log(numberOfClients);
 		console.log(clients);
 	});
-	console.log(numberOfClients);
-	console.log(clients);
+
+	setInterval(function(){
+		io.sockets.emit("connected_users",{numberOfClients:numberOfClients,clients:clients});
+	},1000);
 });
+
