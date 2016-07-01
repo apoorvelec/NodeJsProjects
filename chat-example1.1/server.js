b9c27fd5080d887c11585acfa2e1622e398a17a7
apoorvelec <apoorv@iitgn.ac.in>
2016-07-01 16:04:03 -0400

    private chat prototype successful

--- chat-example1.1/server.js
+++ chat-example1.1/server.js
@@ -4,6 +4,7 @@ var fs = require("fs");
 var portNumber = 3000;
 var numberOfClients = 0;
 var clients = [];
+var clientSockets = [];
 
 
 var server = http.createServer(function(request,response){
@@ -21,10 +22,24 @@ io.sockets.on("connection",function(socket){
 		io.sockets.emit("message_to_client",{message:data["message"], username:socket.username});
 	});
 
+	socket.on("private_message",function(data){
+		//find the target socket id
+		var targetSocketId = "";
+		for(var i=0;i<clientSockets.length;i++){
+			if(data['targetUser']==clientSockets[i]['username']){
+				// then break
+				targetSocketId = clientSockets[i].id;
+			}
+		}
+		// Now that target socket id is found send message
+		io.to(targetSocketId).emit("message_to_client",{message:data["message"],username:socket.username});
+	});
+
 	socket.on("assign_username",function(data){
 		socket.username = data['USERNAME'];
 		console.log(socket.username+" connected ...");
 		clients.push(socket.username);
+		clientSockets.push(socket);
 		numberOfClients++;
 		console.log(numberOfClients);
 		console.log(clients);
@@ -35,6 +50,7 @@ io.sockets.on("connection",function(socket){
 		if(indexOfSocket != -1){
 			// remove the object using splice method
 			clients.splice(indexOfSocket,1);
+			clientSockets.splice(indexOfSocket,1);
 		}
 		numberOfClients--;
 		console.log(numberOfClients);
@@ -43,6 +59,7 @@ io.sockets.on("connection",function(socket){
 
 	setInterval(function(){
 		io.sockets.emit("connected_users",{numberOfClients:numberOfClients,clients:clients});
-	},1000);
+		console.log(clientSockets);
+	},5000);
 });
 
